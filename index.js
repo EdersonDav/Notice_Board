cd0874909b86af8c57d247e6458b919a6948a6a9
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-06-23 19:58:42 -0300

    Create router new notices

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = 5000;
 const app = express();
 const router = require("./routes/routes");
 
+app.use(bodyParser.json());
+
 app.use("/", router);
 
 app.listen(PORT, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+function generateId() {
+  return Math.random().toString(36).replace(".", "");
+}
+
 const notices = [
   {
-    id: Math.random() * 10,
-    Title: "First Notice test",
-    description: "First Description test",
+    id: generateId(),
+    title: "First Notice test",
+    description: "First description test",
   },
   {
-    id: Math.random() * 10,
-    Title: "Second Notice test",
-    description: "Second Description test",
+    id: generateId(),
+    title: "Second Notice test",
+    description: "Second description test",
   },
 ];
 
 //List all notices
 router.get("/all", (req, res) => {
-  res.json(JSON.stringify(notices));
+  res.json(notices);
 });
 
-router.post("/new", (req, res) => {});
+router.post("/new", (req, res) => {
+  let id = generateId();
+  let title = req.body.title;
+  let description = req.body.description;
+
+  notices.push({
+    id,
+    title,
+    description,
+  });
+
+  res.send("Notices create sucess");
+});
 
 module.exports = router;
