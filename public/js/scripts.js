e8cea3d843e4937681363262938f66887abf9c0b
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-06-24 22:02:28 -0300

    Creat new notice front-end

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,11 +14,32 @@ function loadingNotices() {
         let itemNotice = `
         <div class="card" id=${notice.id}>
           <h5 class="carTitle">${notice.title}</h5>
-          <div class="carDescription">${notice.description}</div>
+          <p class="carDescription">${notice.description}</p>
         </div>
       `;
         allNotes += itemNotice;
       });
       document.getElementById("notices").innerHTML = allNotes;
     });
+}
+
+function newPost() {
+  let title = document.querySelector("#title").value;
+  let description = document.querySelector("#description").value;
+  let notice = { title, description };
+  const options = {
+    method: "POST",
+    headers: new Headers({ "content-type": "application/json" }),
+    body: JSON.stringify(notice),
+  };
+  if (title != "" && description != "") {
+    fetch("http://localhost:5000/api/new", options).then((resp) => {
+      console.log(resp);
+      document.querySelector("#title").value = "";
+      document.querySelector("#description").value = "";
+      loadingNotices();
+    });
+  } else {
+    alert("Your title and description cannot empty");
+  }
 }
