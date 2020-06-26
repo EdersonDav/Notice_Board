a1c3d3cf68549e110ab801050900ebac5741c4ab
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-06-25 22:03:48 -0300

    Delete notices

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   loadingNotices();
 });
 
+//div form create new Notice
+let btnNewNotice = document.getElementById("createNewNotice");
+
+//Get all Notices
 function loadingNotices() {
-  fetch("http://localhost:5000/api/all")
+  fetch("http://192.168.15.5:5000/api/all")
     .then((res) => {
       return res.json();
     })
@@ -13,6 +17,19 @@ function loadingNotices() {
       notices.forEach((notice) => {
         let itemNotice = `
         <div class="card" id=${notice.id}>
+          <div class="setings">
+            <button 
+              onclick="setings('${notice.id + "s"}')" 
+              class="dotdotdot">
+              . . .
+            </button>
+            <div id=${notice.id + "s"} class="containerSetings">
+              <button onclick="deleteNotice('${notice.id}')" 
+              class="btnSet">Delete</button>
+              <br />
+              <button class="btnSet">Update</button>
+            </div>
+          </div>
           <h5 class="carTitle">${notice.title}</h5>
           <p class="carDescription">${notice.description}</p>
         </div>
@@ -23,6 +40,7 @@ function loadingNotices() {
     });
 }
 
+//Create New Notice
 function newPost() {
   let title = document.querySelector("#title").value;
   let description = document.querySelector("#description").value;
@@ -33,13 +51,56 @@ function newPost() {
     body: JSON.stringify(notice),
   };
   if (title != "" && description != "") {
-    fetch("http://localhost:5000/api/new", options).then((resp) => {
+    fetch("http://192.168.15.5:5000/api/new", options).then((resp) => {
       console.log(resp);
-      document.querySelector("#title").value = "";
-      document.querySelector("#description").value = "";
+      showOrHideNewNotice();
       loadingNotices();
     });
   } else {
     alert("Your title and description cannot empty");
+  }
+}
+
+//Delete Notice
+function deleteNotice(id) {
+  let noticeid = { id };
+  const options = {
+    method: "DELETE",
+    headers: new Headers({ "content-type": "application/json" }),
+    body: JSON.stringify(noticeid),
+  };
+  fetch("http://192.168.15.5:5000/api/delete", options).then((resp) => {
+    console.log(resp);
+    loadingNotices();
+  });
+}
+
+//Show form create notice
+function showOrHideNewNotice() {
+  if (btnNewNotice.classList.contains("hide")) {
+    btnNewNotice.classList.remove("hide");
+    btnNewNotice.classList.add("show");
+  } else {
+    btnNewNotice.classList.remove("show");
+    btnNewNotice.classList.add("hide");
+    document.querySelector("#title").value = "";
+    document.querySelector("#description").value = "";
+  }
+}
+
+//Show Setings
+function setings(sId) {
+  console.log(sId);
+
+  //div container setings
+  let containerSetings = document.getElementById(sId);
+
+  if (!containerSetings.classList.contains("containerSetingsAnimateDown")) {
+    containerSetings.classList.add("containerSetingsAnimateDown");
+    containerSetings.classList.remove("containerSetingsAnimateUp");
+    containerSetings.style.visibility = "initial";
+  } else {
+    containerSetings.classList.add("containerSetingsAnimateUp");
+    containerSetings.classList.remove("containerSetingsAnimateDown");
   }
 }
